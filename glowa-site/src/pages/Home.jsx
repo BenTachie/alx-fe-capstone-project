90b8352ffa33ca05e8e7ae0503631c00427b6709
BenTachie <tachiebenedict@gmail.com>
2025-10-19 15:15:32 +0200

    refactor: update imports and code style in main.jsx, add Footer import

--- glowa-site/src/pages/Home.jsx
+++ glowa-site/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../components/organisms/NavBar";
 import Hero from "../components/organisms/Hero";
 import Recommended from "../components/organisms/Recommended";
+import Footer from "../components/organisms/Footer";
 
 function Home() {
   return (
